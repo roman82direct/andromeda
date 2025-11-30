a3ee6b98562ff39664dd2ec528ca12bc1d506065
Roman Papenov <roman82sp@yandex.ru>
2025-11-30 13:29:20 +0300

    Extend products.admin

--- andromeda/products/admin.py
+++ andromeda/products/admin.py
@@ -1,10 +1,71 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Brand, Collection, Image, Product
+from .base_models.categories_groups import Group, MainCategory, SecondCategory
+
+
+class CommonSettingsAdmin(admin.ModelAdmin):
+    """
+    Базовый класс админ-модели products.
+
+    Определяет общие настройки интерфейса отображения объектов приложения
+    в админ-панели.
+    """
+
+    list_display_links = ('title',)
+    list_per_page = 10
+
+
+class CommonCategoriesFieldsAdmin(CommonSettingsAdmin):
+    """
+    Базовый класс админ-модели products.
+
+    Определяет настройки отображения полей категорий в админ-панели.
+    """
+
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+    )
+
+
+@admin.register(SecondCategory)
+class SecondCategoryAdmin(CommonCategoriesFieldsAdmin):
+    pass
+
+
+class SecondCategoryInline(admin.TabularInline):
+    model = SecondCategory
+    extra = 1
+    show_change_link = True
+
+
+@admin.register(MainCategory)
+class MaincategoryAdmin(CommonCategoriesFieldsAdmin):
+    inlines = (SecondCategoryInline,)
+
+
+class MainCategoryInline(admin.TabularInline):
+    model = MainCategory
+    extra = 1
+    show_change_link = True
+
+
+@admin.register(Group)
+class GroupCategoryAdmin(CommonCategoriesFieldsAdmin):
+    inlines = (MainCategoryInline,)
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 6
+    show_change_link = True
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(CommonSettingsAdmin):
+    inlines = (ImageInline,)
     list_display = (
         'title',
         'item_number',
@@ -13,6 +74,7 @@ class ProductAdmin(admin.ModelAdmin):
         'brand',
         'second_category',
         'collection',
+        'is_published',
     )
     search_fields = (
         'title',
@@ -22,6 +84,15 @@ class ProductAdmin(admin.ModelAdmin):
         'brand',
         'second_category',
         'collection',
-        'second_category__main_category',
-        'second_category__main_category__group',
     )
+    list_editable = ('is_published', 'second_category',)
+
+
+@admin.register(Brand)
+class BrandAdmin(CommonCategoriesFieldsAdmin):
+    pass
+
+
+@admin.register(Collection)
+class CollectionAdmin(CommonCategoriesFieldsAdmin):
+    pass
