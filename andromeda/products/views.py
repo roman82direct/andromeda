ef92d28060d84111baa9dff47d8d8ebe94e0a690
Roman Papenov <roman82sp@yandex.ru>
2025-12-11 17:26:37 +0300

    Add collections on main page

--- andromeda/products/views.py
+++ andromeda/products/views.py
@@ -1,17 +1,35 @@
 from django.contrib.auth import get_user_model
-
+from django.db.models import CharField, OuterRef, Subquery
 from django.views.generic import ListView
 
-from .models import Product
+from .models import Collection, Image, Product
 
 
 User = get_user_model()
 
 
-class CollectionsList(ListView):
+class MainList(ListView):
 
     template_name = 'products/index.html'
     context_object_name = 'products'
 
     def get_queryset(self):
         return Product.card_objects.get_card_products()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        main_img_subquery = (
+            Image.objects
+            .filter(product__collection=OuterRef('pk'), is_main=True)
+            .order_by('-update_at')
+            .values('img_url')[:1]
+        )
+        collections = (
+            Collection.objects.filter(
+                is_published=True
+            ).annotate(
+                img_url=Subquery(main_img_subquery, output_field=CharField())
+            )
+        )
+        context['collections'] = collections
+        return context
