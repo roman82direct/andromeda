b2b7b306e457a2cb584c52f9113033bc74b9e73d
Grigory Morozov <morozehc@yandex.ru>
2026-01-18 18:51:50 +0300

    feat(component Logo):  create logic Logo with possibility to go to home page

---
+++ frontend/src/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import { LogoUI } from "../ui/logo";
+import type {FC} from 'react';
+
+export const Logo:FC = () => {
+  return (
+          <Link aria-label='логотип сайта - переход на главную страницу сайта'  to={'/'}>
+            <LogoUI/>
+          </Link>
+        )
+}
