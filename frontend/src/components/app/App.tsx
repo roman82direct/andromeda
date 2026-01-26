8d26262fd1fd9f2050190b20cb31a265bc262b0a
Grigory Morozov <morozehc@yandex.ru>
2026-01-26 13:26:11 +0300

    feat(frontend): created routes in App for cart, profile, login and favorite

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -5,6 +5,12 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<div>Здесь будет главная страница</div>} />
+        <Route path='/catalog-main' element={<div>Основная страница товаров</div>}/>
+        <Route path='/login' element={<div>Страница входа на сайт</div>}/>
+        {/*  профиль, корзина, избранное д б доступны тольок авторизованным пользователям и быть вложенны в защищенный маршрут */}
+        <Route path='/favorite' element={<div>Избранное</div>}/>
+        <Route path='/cart' element={<div>Корзина</div>}/>
+        <Route path='/profile'element={<div>Профиль</div>}/>
       </Routes>
     </>
   );
