1cb694467f5d96f4f87d919549f2bd28d150ac42
Grigory Morozov <morozehc@yandex.ru>
2026-01-26 15:31:30 +0300

    feat(frontend): add routes for catalog goods, current clicked page - good with id, register, login, forgot-password, reset-password

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -4,13 +4,21 @@ function App() {
   return (
     <>
       <Routes>
+        {/* примерный план маршрутов */}
         <Route path="/" element={<div>Здесь будет главная страница</div>} />
-        <Route path='/catalog-main' element={<div>Основная страница товаров</div>}/>
-        <Route path='/login' element={<div>Страница входа на сайт</div>}/>
+        <Route path='/catalog' element={<div>Основная страница товаров</div>}/>
+        <Route path='/catalog/:id' element={<div>Страница конкретного товара</div>}/>
         {/*  профиль, корзина, избранное д б доступны тольок авторизованным пользователям и быть вложенны в защищенный маршрут */}
         <Route path='/favorite' element={<div>Избранное</div>}/>
         <Route path='/cart' element={<div>Корзина</div>}/>
         <Route path='/profile'element={<div>Профиль</div>}/>
+
+        <Route path='/register'element={<div>Регистрация</div>}/>
+        <Route path='/login' element={<div>Страница входа на сайт</div>}/>
+        <Route path='/forgot-password'element={<div>Регистрация</div>}/>
+        <Route path='/reset-password'element={<div>Регистрация</div>}/>
+
+        <Route path='*' element={<div>404 - Страница не найдена</div>}/>
       </Routes>
     </>
   );
