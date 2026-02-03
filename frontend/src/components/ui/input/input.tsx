2f248427e4f29a0134f7193a912e6b8ce299ac40
Grigory Morozov <morozehc@yandex.ru>
2026-02-03 14:50:49 +0300

    feat(frontend): create component InputUI

---
+++ frontend/src/components/ui/input/input.tsx
@@ -0,0 +1,60 @@
+import {type ChangeEvent, type FC} from 'react';
+import styles from './input.module.css';
+import { IconButtonUI } from '../icon-button';
+import clsx from 'clsx';
+
+type TInputUIProps = {
+  isDisabled?: boolean,
+  onChange: (e:ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  type: 'text' | 'password' | 'email' | 'search' | 'tel' | 'url';
+  placeHolder: string;
+  onClear: () => void;
+  nameInput: string;
+  errorMessage?: string
+  variant:'primary' | 'secondary'
+}
+
+export const InputUI:FC<TInputUIProps> = (
+  {
+    isDisabled, 
+    onChange, 
+    value, 
+    type,  
+    placeHolder, 
+    onClear,
+    nameInput,
+    errorMessage,
+    variant
+  }) => {
+  const inputClassName = clsx(
+      styles["input"], 
+      variant === 'primary' ? styles['input-primary'] : styles['input-secondary'],
+    ) 
+  return (
+          <div className={styles['container-input']}>
+            <div className={styles['input-wrapper']}>
+              <input
+                disabled = {isDisabled}
+                className={inputClassName}
+                onChange={onChange}
+                name={nameInput}
+                value={value}
+                type={type}
+                placeholder={placeHolder}
+              />
+              {errorMessage &&  <p className={styles['input-error']}>{errorMessage}</p>}
+            </div>
+            <div className={styles['wrapper-btn-clear']}>
+              <IconButtonUI
+                onClick={onClear}
+                isActive={false}
+                iconClass={"close"}
+                sizeIcon={15}
+                colorIcon={variant}
+                isDisabled={isDisabled}
+              />
+          </div>
+        </div>
+  )
+}
