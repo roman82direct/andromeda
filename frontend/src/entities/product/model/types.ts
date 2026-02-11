028ec831ea6d84e6a3c4e4318cbe526b8f3691dc
Grigory Morozov <morozehc@yandex.ru>
2026-02-11 23:22:50 +0300

    feat(frontend): create entity for Product + adapter from DTO to Product

---
+++ frontend/src/entities/product/model/types.ts
@@ -0,0 +1,26 @@
+//  сущности которые будем использовать в приложении
+export interface IProduct {
+  id: number;
+  articul: string;
+  title: string;
+  description: string;
+  price: number; // в отличие от DTO берем number вместо string
+  isPublished: boolean;
+  collection: IInfoPorduct;
+  brand: IInfoPorduct;
+  secondCategory: string;
+  images: IImageProduct[];
+  createdAt: Date; // ISO дата
+  updateAt: Date; // ISO дата
+}
+
+export interface IInfoPorduct {
+  title: string;
+  description: string;
+}
+
+export interface IImageProduct {
+  imgUrl: string;
+  isMain: boolean;// ??
+  isPack: boolean;// ??
+}
