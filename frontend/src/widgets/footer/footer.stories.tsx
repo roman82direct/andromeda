341f0f0fc8efd5fe13bd955d4ae8b5efc84f5d69
Grigory Morozov <morozehc@yandex.ru>
2026-02-11 17:25:43 +0300

    feat(frontend): create Footer component

---
+++ frontend/src/widgets/footer/footer.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Footer} from './index';
+import { MemoryRouter } from "react-router-dom";
+
+
+const meta: Meta<typeof Footer> = {
+  component: Footer,
+  title: "widgets/footer/Footer",
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+
+export const footerUI: Story = {
+  args: {},
+};
--- frontend/src/widgets/footer/footer.tsx
+++ frontend/src/widgets/footer/footer.tsx
@@ -1,11 +1,10 @@
 import { FooterUI } from "./ui";
 import {type FC} from 'react';
-
-
+import { navIconsForFooter } from "./data/footer-icons";
+import {footerLinks} from './data/footer-links'
 
 export const Footer:FC = ()=>{
-
   return(
-    <FooterUI  columnsListsLinks={[]} socialLinksIcons={[]}/>
+    <FooterUI  columnsListsLinks={footerLinks} socialLinksIcons={navIconsForFooter}/>
   )
 }
