290a3a3ef7f6d109eb26b3eccc52f195669579be
Grigory Morozov <morozehc@yandex.ru>
2026-02-17 21:28:08 +0300

    feat(app): connect global store to our App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
+import "./app/styles/index.css";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./app/providers/store/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+   </BrowserRouter>
   </StrictMode>,
 );
