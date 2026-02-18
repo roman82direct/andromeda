a5c2d4596c84bfe2180bc2b86cfbc0b163eb214c
Grigory Morozov <morozehc@yandex.ru>
2026-02-18 21:27:39 +0300

    fix(api): correct func checkResponse (status 200 or not)

--- frontend/src/shared/api/base.ts
+++ frontend/src/shared/api/base.ts
@@ -1,22 +1,2 @@
-//  обработка ошибки промиса
-// export const checkResponse = <T>(res: Response): Promise<T> =>
-//   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
-
-
-export const checkResponse = async <T>(res: Response): Promise<T> => {
-  try {
-    // Пытаемся распарсить тело ответа как JSON
-    const data =  await res.json();
-    if(!res.ok) {
-        return Promise.reject(data) // если статус не 200 - возвращает эту ошибку
-    }
-    return data
-  } catch (err) {
-    //  если JSON не удалось распарсить - напр сервер вернул некорректный json или рустое тело ответа
-    // нам нужен гарантированный объект ошибки
-    return Promise.reject({
-      messages:'unknown Error from Server',
-      originalError: err
-    })
-  }
-}
+export const checkResponse = <T>(res: Response): Promise<T> =>
+  res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
