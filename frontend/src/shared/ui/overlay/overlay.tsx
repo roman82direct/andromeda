228d14c8d2ad8b94fa527fe060ccf8a4304ad878
Grigory Morozov <morozehc@yandex.ru>
2026-02-22 19:50:39 +0300

    feature(shared): create ui overlay

---
+++ frontend/src/shared/ui/overlay/overlay.tsx
@@ -0,0 +1,6 @@
+import styles from './overlay.module.css';
+
+
+export const OverlayUI = ({ onClick }: { onClick: () => void }) => {
+  return <div onClick={onClick} className={styles.overlay}></div>
+}
