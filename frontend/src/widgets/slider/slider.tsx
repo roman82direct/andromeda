b3c1b759711896431749256b79cb25e2e21a9a25
Grigory Morozov <morozehc@yandex.ru>
2026-04-17 16:36:17 +0300

    refactor(home slider): change name variable

--- frontend/src/widgets/slider/slider.tsx
+++ frontend/src/widgets/slider/slider.tsx
@@ -7,7 +7,7 @@ import { sliderStore } from "./model/sliderStore";
 import { SliderContext } from "./utils/contexts";
 
 export const SliderComponent = () => {
-  const [sliders, setSliders] = useState<TSlideItem[]>([]);
+  const [slides, setSliders] = useState<TSlideItem[]>([]);
 
   // загружаем информацию о слайдах в наш компонент
   //  если запрос на сервер можно создать стор с редукс
@@ -27,25 +27,25 @@ export const SliderComponent = () => {
     handleChangeSlide, // // Функция для кнопок "Вперед" и "Назад"
     // автоматич переключение слайдов
     toggleIntervalSlide,
-  } = useChangeSlide(sliders);
+  } = useChangeSlide(slides);
 
   //  работа с пагинацией слайдов - сколько кнопок пагинации показываем согласно макету
   const pagePagSize = 3;
   // вычисляем индексы пагинации так чтобы они совпали со номерами индексов слайдов в sliders
   // чтобы можно было показать тек слайд, слайд перед ним и после него(те тройку слайдов где есть тек показ слайд)
-  const currentIndexesPag = getPagIndexes(indexSlide, pagePagSize, sliders);
+  const currentIndexesPag = getPagIndexes(indexSlide, pagePagSize, slides);
   // !!!!!!!!
-  if (!sliders.length) return <div>Сделать лоадер загрузки</div>;
+  if (!slides.length) return <div>Сделать лоадер загрузки</div>;
   //  разделение контекстов ???
   return (
     <SliderContext.Provider
       value={{
         slideNumber: indexSlide,
-        slides: sliders,
+        slides: slides,
         dotsPag: currentIndexesPag,
         setIndexSlide: setIndexSlide,
         // для пагинации если слайдчерный чтобы тема точек было белая допустим
-        currentSlideTheme: sliders[indexSlide].typeTheme,
+        currentSlideTheme: slides[indexSlide].typeTheme,
         handleChangeSlide,
       }}
     >
