d1ce826de7d352c1d60abbb213c611d95a6cc846
Grigory Morozov <morozehc@yandex.ru>
2026-04-23 20:24:13 +0300

    feat(home-page):add h1 hidden for home page

--- frontend/src/pages/home-page/ui/home-page.tsx
+++ frontend/src/pages/home-page/ui/home-page.tsx
@@ -5,6 +5,7 @@ import type { FC } from "react";
 export const HomePageUI: FC = () => {
   return (
     <div className={styles.home}>
+      <h1 className="visually-hidden">Andromeda Store — магазин керамической посуды и аксессуаров для дома</h1>
       <section className={styles["home-banner"]}>
         <Slider />
       </section>
