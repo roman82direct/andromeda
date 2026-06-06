053d978447f02ad8850b116f5d6db15f126ae3ee
Grigory Morozov <morozehc@yandex.ru>
2026-06-07 01:05:49 +0300

    fix(slider): correct autoplay slides condition if app  dont need autoplay

--- frontend/src/widgets/slider/hooks/useAutoPlayShowSlides.ts
+++ frontend/src/widgets/slider/hooks/useAutoPlayShowSlides.ts
@@ -16,18 +16,25 @@ export const useAutoPlayShowSlides = (
       const directionRef = useRef(true);
     //  сделать отдельный хук для автоплея
       useEffect(() =>{
-         //  меняем направления автоплея если слайдер достиг первого или послед слайда
-               if(indexSlide ===  slidesArrLength-1){
+       //  меняем направления автоплея если слайдер достиг первого или послед слайда
+      //   
+      // чтобылишний раз юзэффект не работал если стоит бесконечнй цикл или автоплея нет
+      //  нам этот юзэффект не нужен 
+      if(infiniteLoop || !autoPlay) return;
+
+         if(indexSlide ===  slidesArrLength-1){
                     directionRef.current = false
                 }
-                 if(indexSlide ===0){
-                    directionRef.current = true
-    
-              }
+        if(indexSlide ===0){
+          directionRef.current = true
+
+        }
       },[
         indexSlide,
-        slidesArrLength
-      ])
+        slidesArrLength,
+        infiniteLoop,
+        autoPlay
+        ])
       //  автоматич показ слайдов
       useEffect(() => {
         // autoPlay  переменная должна задаваться обработчиком и если это нужно нам
