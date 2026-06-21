e839fd65496da19aca6b1790eace4587b4607b60
Grigory Morozov <morozehc@yandex.ru>
2026-06-21 16:44:32 +0300

    feat(catalog-tabs): add key for rendere map every tab and set component AppImage for rendering image with different formats

--- frontend/src/widgets/catalog-tabs/utils/render-tab-content.tsx
+++ frontend/src/widgets/catalog-tabs/utils/render-tab-content.tsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom"
 import type { TCatalogContent } from "../types"
+import { AppImage } from "@/shared/ui/app-image/app-image";
 
 export const renderTabContent = (tabContent:  TCatalogContent[]) => {
   return <>
     {
       //  функцию можно  масштабировать под разный контент
-      tabContent.map((contentItem)=> (
+      tabContent.map((contentItem, index)=> {
+        const key = contentItem.link || index;
         //  создать для ссылки переход
-        <Link to=''>
+       return ( 
+        // сделать navlink вместо link для активной ссылки
+        <Link key={key} to={contentItem.link}>
           {/* подумать насчет подпписи */}
+          {/* здесь нужно масштабировать контент */}
           <figure>
-            <img src={contentItem.image} alt={contentItem.descpImage} />
-            <figcaption>{contentItem.descpImage}</figcaption>
+            <picture>
+              <AppImage srcImage={contentItem.image} descrImage={`изображение ${contentItem.descpImage}`} />
+            </picture>
+            {contentItem.descpImage && (<figcaption>{contentItem.descpImage}</figcaption>)}
           </figure>
          
-        </Link> 
-      ))
+          </Link> 
+        )
+      })
     }
   </>
 }
