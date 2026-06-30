67acd4e60dc8a5722655dda33d241739026e65c6
Grigory Morozov <morozehc@yandex.ru>
2026-06-30 12:13:32 +0300

    feat(app-image): add default styles css for iamge

--- frontend/src/shared/ui/app-image/app-image.tsx
+++ frontend/src/shared/ui/app-image/app-image.tsx
@@ -1,4 +1,6 @@
 import type { TImage } from "@/shared/types/types";
+import styles from './app-image.module.css';
+import clsx from "clsx";
 
 export type ImageProps = {
   descrImage?: string;
@@ -9,6 +11,8 @@ export type ImageProps = {
 //  придумать дефолт блок стайл для картинки?
 
 export const AppImage = ({ descrImage, srcImage, className }: ImageProps) => {
+  const styleClass = className ?  className : '';
+  const defaultClass = 'defaultClassImg';
   return (
     <picture>
       {srcImage.avif && (
@@ -24,7 +28,7 @@ export const AppImage = ({ descrImage, srcImage, className }: ImageProps) => {
         />
       )}
       <img
-        className={className}
+        className={clsx(styles[defaultClass],styles[styleClass] )}
         src={srcImage.jpg["1x"]}
         srcSet={`${srcImage.jpg["2x"]} 2x`}
         alt={descrImage ? descrImage : "здесь дб картинка"}
