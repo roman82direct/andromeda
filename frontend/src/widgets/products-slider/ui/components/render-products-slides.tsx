9d8383721dfed3bdea47825d2ea4428204f320a1
Grigory Morozov <morozehc@yandex.ru>
2026-08-28 20:23:08 +0300

    fix: add uniq id key to renderProductSlides(CardProduct)

--- frontend/src/widgets/products-slider/ui/components/render-products-slides.tsx
+++ frontend/src/widgets/products-slider/ui/components/render-products-slides.tsx
@@ -1,10 +1,13 @@
 import { CardProduct, type TCardProduct } from "@/entities/product"
 import styles from '../products-slider.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 export const renderProductSlides = (slidesProducts: TCardProduct[] )=>{
+  
   return slidesProducts.map((slideProduct)=>{
-    return <div className={styles['card-slide-wrapper']}>
-              <CardProduct key={slideProduct.id} {...slideProduct}/>
+    const idCard = uuidv4();
+    return <div key={idCard} className={styles['card-slide-wrapper']}>
+              <CardProduct  {...slideProduct}/>
           </div>
    
   })
